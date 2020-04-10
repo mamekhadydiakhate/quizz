59d981d0af64c9ffde99c8f90f892d4b130f9345
mamekhadydiakhate <62646929+mamekhadydiakhate@users.noreply.github.com>
2020-04-10 16:17:34 +0000

    Add files via upload

---
+++ QUIZZ/pageconnexion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>page connexion</title>
+	<link rel="stylesheet" href="miniprojet.css">
+</head>
+<body>
+<header  style="background-color: #07403a; ">
+	<div>
+		<img src="projet/logo-QuizzSA.png" style="width: 50px; height: 60px;">
+		<center>
+			<h1 style="margin-top: -2%; color: white ; ">Le Plaisir de jouer</h1>
+		</center>
+	</div>
+</header>
+	<div class="fond">
+		<center>
+		<form action="pageconnexion.php" method="POST">
+		<h4>Loging Form</h4>
+		<div class="loging">
+			<input type="text" name="login" placeholder="login">
+		</div>
+		</form>
+		</center>	
+	</div>
+</body>
+</html>
