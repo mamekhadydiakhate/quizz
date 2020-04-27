3aeb3c3af046d002fe1a65e99391f7c0ccbdad60
mamekhadydiakhate <62646929+mamekhadydiakhate@users.noreply.github.com>
2020-04-27 11:43:00 +0200

    Add files via upload

---
+++ niveau.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="projet.css">
+<div class="plan">
+    <div class="plan-header">
+        <div class="titre">
+            <div class="avatar1">
+                <img src="" alt="" id="avatar1" style="min-width: 4rem;min-height: 4rem; border-radius: 100%; border: solid 1px white; float: left; margin-left:-350px" >
+            </div>
+            <h3 style="color:white ;float:left; margin-right:200px">BIENVENUE SUR LA PLATEFORME DE JEU DE QUIZZ
+                    JOUER ET TESTER VOTRE NIVEAU DE CULTURE GÉNÉRALE</h3>
+        </div>
+            <div class="deconnexion">
+        <form action="" method="POST">
+            <input type="submit" name ="deconnexion" value="deconnexion">
+            </div>
+        </form>
+    </div>
+    <div class="question">
+                <div class="quizz">
+                    <div class="result">
+                        <input type="text" name="result">
+                    </div>
+                <div class="precedent">
+                    <input type="submit" name="precedent" value="precedent">
+                </div>
+                <div class="suivant">
+                    <input type="submit" name="suivant" valu="suivant">
+                </div>
+                </div>
+                <div class="scor">
+         
+                </div>
+     </div>
+    
+</div>   
